aea662f7acc23219eda3a37046a47a7b6be50571
Jinx <1012140020@qq.com>
2022-05-19 23:56:14 +0800

    update Scene and Customer update some db and resource

---
+++ src/main/java/se/xmut/trahrs/mapper/SceneMapper.java
@@ -0,0 +1,16 @@
+package se.xmut.trahrs.mapper;
+
+import se.xmut.trahrs.domain.model.Scene;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 景点信息 Mapper 接口
+ * </p>
+ *
+ * @author 作者
+ * @since 2022-05-19
+ */
+public interface SceneMapper extends BaseMapper<Scene> {
+
+}
