bc5789981f4d99b6a80698e686db4dac8bdc5478
shinelon <2457751533@qq.com>
2022-05-23 23:48:08 +0800

    酒店信息根据不同类型查找

--- src/main/java/se/xmut/trahrs/service/HotelInfoService.java
+++ src/main/java/se/xmut/trahrs/service/HotelInfoService.java
@@ -2,8 +2,11 @@ package se.xmut.trahrs.service;
 
 import se.xmut.trahrs.domain.model.HotelInfo;
 import com.baomidou.mybatisplus.extension.service.IService;
+import se.xmut.trahrs.domain.model.HotelRoomInfo;
+import se.xmut.trahrs.domain.vo.HotelInfoVo;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -14,5 +17,12 @@ import java.util.List;
  * @since 2022-05-20
  */
 public interface HotelInfoService extends IService<HotelInfo> {
+
+    /**
+     * HotelInfoVo分页助手
+     * @param hotelInfoVoList 需要被分页的HotelInfoVo
+     * @return 分页完成的map
+     */
+    public Map<String, Object> HotelInfoVoPage(List<HotelInfoVo> hotelInfoVoList, Integer pageNum, Integer pageSize);
     List<HotelInfo> findHotelByTypeAndKeyTagAndName(String type, String key_tag, String name_brand);
 }
--- src/main/java/se/xmut/trahrs/service/impl/HotelInfoServiceImpl.java
+++ src/main/java/se/xmut/trahrs/service/impl/HotelInfoServiceImpl.java
@@ -1,14 +1,18 @@
+
 package se.xmut.trahrs.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import se.xmut.trahrs.domain.model.HotelInfo;
+import se.xmut.trahrs.domain.vo.HotelInfoVo;
 import se.xmut.trahrs.mapper.HotelInfoMapper;
 import se.xmut.trahrs.service.HotelInfoService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 import springfox.documentation.annotations.Cacheable;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -25,8 +29,31 @@ public class HotelInfoServiceImpl extends ServiceImpl<HotelInfoMapper, HotelInfo
     HotelInfoMapper hotelInfoMapper;
 
     @Override
+    public Map<String, Object> HotelInfoVoPage(List<HotelInfoVo> hotelInfoVoList, Integer pageNum, Integer pageSize) {
+
+        List<HotelInfoVo> hotelInfoPage =  hotelInfoVoList.subList((pageNum-1)*pageSize, (pageNum-1)*pageSize+pageSize);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("records", hotelInfoPage);
+        map.put("total", hotelInfoVoList.size());
+        map.put("size", pageSize);
+        map.put("current", pageNum);
+        map.put("pages", hotelInfoVoList.size()%pageSize==0 ? hotelInfoVoList.size()/pageSize : hotelInfoVoList.size()/pageSize +1);
+
+        return map;
+    }
+
+    @Override
     public List<HotelInfo> findHotelByTypeAndKeyTagAndName(String type, String key_tag, String name_brand) {
         List<HotelInfo> hotelInfoList=hotelInfoMapper.findHotelByTypeAndKeyTagAndName(type,key_tag,name_brand);
         return hotelInfoList;
     }
 }
+
+
+
+
+
+
+
+
