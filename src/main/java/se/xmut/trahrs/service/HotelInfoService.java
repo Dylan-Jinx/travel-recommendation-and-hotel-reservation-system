3922699371022f3b9d44dd11efc6977b6afcf791
shinelon <2457751533@qq.com>
2022-05-23 23:31:47 +0800

    酒店信息根据不同类型查找

--- src/main/java/se/xmut/trahrs/service/HotelInfoService.java
+++ src/main/java/se/xmut/trahrs/service/HotelInfoService.java
@@ -3,6 +3,8 @@ package se.xmut.trahrs.service;
 import se.xmut.trahrs.domain.model.HotelInfo;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
  * <p>
  *  服务类
@@ -12,5 +14,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2022-05-20
  */
 public interface HotelInfoService extends IService<HotelInfo> {
-
+    List<HotelInfo> findHotelByTypeAndKeyTagAndName(String type, String key_tag, String name_brand);
 }
--- src/main/java/se/xmut/trahrs/service/impl/HotelInfoServiceImpl.java
+++ src/main/java/se/xmut/trahrs/service/impl/HotelInfoServiceImpl.java
@@ -1,11 +1,14 @@
 package se.xmut.trahrs.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import se.xmut.trahrs.domain.model.HotelInfo;
 import se.xmut.trahrs.mapper.HotelInfoMapper;
 import se.xmut.trahrs.service.HotelInfoService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 import springfox.documentation.annotations.Cacheable;
+
+import java.util.List;
 
 /**
  * <p>
@@ -18,4 +21,12 @@ import springfox.documentation.annotations.Cacheable;
 @Service
 public class HotelInfoServiceImpl extends ServiceImpl<HotelInfoMapper, HotelInfo> implements HotelInfoService {
 
+    @Autowired
+    HotelInfoMapper hotelInfoMapper;
+
+    @Override
+    public List<HotelInfo> findHotelByTypeAndKeyTagAndName(String type, String key_tag, String name_brand) {
+        List<HotelInfo> hotelInfoList=hotelInfoMapper.findHotelByTypeAndKeyTagAndName(type,key_tag,name_brand);
+        return hotelInfoList;
+    }
 }
