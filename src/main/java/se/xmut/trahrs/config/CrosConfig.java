72a02c725c9ce046736b5c57cf43fc29b7fb72db
Jinx <1012140020@qq.com>
2022-05-26 22:24:41 +0800

    DruidConfig

--- src/main/java/se/xmut/trahrs/config/CrosConfig.java
+++ src/main/java/se/xmut/trahrs/config/CrosConfig.java
@@ -3,13 +3,8 @@ package se.xmut.trahrs.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Collections;
 
 @Configuration
 public class CrosConfig implements WebMvcConfigurer {
